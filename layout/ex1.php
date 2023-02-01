90939101846c09713df2c110d1fc46cd291bb834
rushani22000 <118686503+rushani22000@users.noreply.github.com>
2023-02-01 10:30:14 +0200

    Initial commit

---
+++ layout/ex1.php
@@ -0,0 +1,21 @@
+<?php include "header.php" ?>
+
+<h3> Write a simple PHP script to print your information (Name and your groupid). </h3>
+    
+        <?php //OPENING TAG        
+        echo "Rushani Madhumali <br>";
+        echo "Group 11";
+        ?>
+
+    <h3> 3.2 Write PHP code to display the following message. </h3>
+    
+        <?php
+        echo "Hello world! My name is \"David\"";
+        ?>
+
+    <h3> Current Date </h3>
+        <?php
+        echo date("d.m.Y")
+        ?>
+
+<?php include "footer.php" ?>
