d1f540a6f05c75c029292633400308d6d89a45cd
rushani22000 <118686503+rushani22000@users.noreply.github.com>
2023-02-01 11:23:26 +0200

    add some chenges

--- layout/ex1.php
+++ layout/ex1.php
@@ -17,5 +17,43 @@
         <?php
         echo date("d.m.Y")
         ?>
+ <h3> 3.4 Variable in php </h3>
+ <?php $title1 = "php is interesting";
+ echo "<h1>" .$title1 . "</h1>";
+ ?>
+
+
+ <h3> 3.5 table and Variable </h3>
+ <?php
+ $g1 = 5;
+ $g2 = 4;
+ $g3 = 5;
+ echo "
+ <table>
+ <tr>
+ <th> S.N </th> <th> Name </th> <th> Grade </th>
+ </tr>
+
+ <tr>
+ <td> 1 </td> <td> Pekka </td> <td> $g1 </td>
+ </tr>
+
+ <tr>
+ <td> 2 </td> <td> Johana </td> <td> $g2 </td>
+ </tr>
+
+ <tr>
+ <td> 3 </td> <td> Johan </td> <td> $g3 </td>
+ </tr>
+ 
+ </table>";
+ ?>
+
+ <h3> 4 Screenshot of the development environment</h3>
+ <img src="" alt="">
+
 
 <?php include "footer.php" ?>
+
+
+
