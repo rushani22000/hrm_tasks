d9ae0f6f21919e8d98edae337f7154c188477a51
rushani22000 <118686503+rushani22000@users.noreply.github.com>
2023-02-09 11:39:54 +0200

    modified header

---
+++ layout/array.php
@@ -0,0 +1,42 @@
+<?php include "header.php" ?>
+
+<form action=""  method="GET">
+    <input type="number" placeholder="Enter your first number" name="num1" required>
+    <input type="number" placeholder="Enter your second number" name="num2" required>
+    <select name="operator">
+        <option value="add">Add</option>
+        <option value="sub">Subract</option>
+        <option value="mul">Multiply</option>
+        <option value="div">Divide</option>
+    </select>
+    <input type="submit" name="cal"  value="Calculate">
+</form>
+
+<?php
+if (isset($_GET["cal"])){
+    $num1 = $_GET["num1"];
+    $num2 = $_GET["num2"];
+    $operator =  $_GET["operator"];
+     switch ($operator){
+        case "add":
+            $result = $num1+$num2;
+            break;
+        case "sub":
+            $result = $num1-$num2;
+            break;
+        case "mul":
+            $result = $num1*$num2;
+            break;
+        case "div":
+            $result = $num1/$num2;
+            break;
+        default:
+            $result = "Error:you have not selected the correct operator";
+}
+}
+
+if (isset($result)){
+    echo "<h2> Result: $result</h2>";
+}
+?>
+<?php include "footer.php" ?>
--- layout/header.php
+++ layout/header.php
@@ -32,10 +32,13 @@
           <a class="nav-link" href="ex1.php">Exercise 1</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="variable.php">Exercise 3</a>
+          <a class="nav-link" href="variable.php">Variable</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="controlflow.php">Exercise 4</a>
+          <a class="nav-link" href="controlflow.php">Controlflow</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="array.php">Array</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="aboutus.php">About Us</a>
