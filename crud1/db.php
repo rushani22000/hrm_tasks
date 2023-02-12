86f25844813124bab4947251bcc4e40b501c4f82
rushani22000 <118686503+rushani22000@users.noreply.github.com>
2023-02-12 16:51:48 +0200

    added create.php

---
+++ crud1/db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "db";
+$username = "capp1";
+$password = "password";
+$dbname = "capp1";
+// creating connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//check the connnection
+if($conn->connect_errno){
+    die("connection faild:".$conn->connect_errno);
+}
+ ?>
